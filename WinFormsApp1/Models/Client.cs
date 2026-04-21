a867d97bbd914da6e1f22de9f7371928dbb1e925
Gabriel Silva <gaabdev42@gmail.com>
2026-04-20 22:40:34 -0400

    Adding form

---
+++ WinFormsApp1/Models/Client.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp1.Models
+{
+    public class Client
+    {
+        public int id;
+        
+        p
+    }
+}
